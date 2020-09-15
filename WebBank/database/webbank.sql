12f06969240cc257af07cd5ed4469167f6dbe911
DanielAndresClavijo <contrutoxd@gmail.com>
2020-09-14 20:29:20 -0500

    Actualizacion bd

--- WebBank/database/webbank.sql
+++ WebBank/database/webbank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-09-2020 a las 22:58:13
+-- Tiempo de generación: 15-09-2020 a las 03:27:31
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.9
 
@@ -21,10 +21,40 @@ SET time_zone = "+00:00";
 -- Base de datos: `webbank`
 --
 
-CREATE SCHEMA IF NOT EXISTS `webbank` CHARACTER SET utf8 COLLATE utf8_spanish_ci;
-USE `webbank` ;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historial`
+--
+
+CREATE TABLE `historial` (
+  `id_historial` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_usuario2` int(11) DEFAULT NULL,
+  `id_transacion` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_transacion`
+--
+
+CREATE TABLE `tipo_transacion` (
+  `id_transacion` int(11) NOT NULL,
+  `nombre_transacion` varchar(50) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tipo_transacion`
+--
+
+INSERT INTO `tipo_transacion` (`id_transacion`, `nombre_transacion`) VALUES
+(1, 'Transferir'),
+(2, 'Depositar'),
+(3, 'Retirar');
 
 -- --------------------------------------------------------
 
@@ -32,7 +62,7 @@ USE `webbank` ;
 -- Estructura de tabla para la tabla `usuario`
 --
 
-CREATE TABLE `webbank`.`usuario` (
+CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL,
   `numero_cedula` varbinary(50) NOT NULL,
   `contrasena` varbinary(50) NOT NULL,
@@ -44,21 +74,33 @@ CREATE TABLE `webbank`.`usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Estructura de tabla para la tabla `historial`
+-- Volcado de datos para la tabla `usuario`
 --
 
-CREATE TABLE `webbank`.`historial` (
-  `id_historial` int(11) NOT NULL,
-  `id_usuario` int(11) NOT NULL,
-  `cantidad` int(11) NOT NULL,
-  `nombre_transacion` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
-  `fecha` date NOT NULL
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+INSERT INTO `usuario` (`id_usuario`, `numero_cedula`, `contrasena`, `primer_nombre`, `segundo_nombre`, `primer_apellido`, `segundo_apellido`, `saldo`) VALUES
+(1, 0x4d5441774e6a49304e6a63794f513d3d, 0x52474675615756734d54497a, 0x5247467561575673, 0x5157356b636d567a, 0x51327868646d6c7162773d3d, 0x52326c795957786b62773d3d, 0x32303030),
+(2, 0x4d54497a4e4455324e7a6735, 0x536d6876626b52765a5445794d773d3d, 0x536d687662673d3d, NULL, 0x5247396c, NULL, 0x353030);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `historial`
+--
+ALTER TABLE `historial`
+  ADD PRIMARY KEY (`id_historial`,`id_usuario`,`id_transacion`),
+  ADD UNIQUE KEY `id_historial_UNIQUE` (`id_historial`),
+  ADD KEY `fk_usuario_has_transacion_transacion1_idx` (`id_transacion`),
+  ADD KEY `fk_usuario_has_transacion_usuario1_idx` (`id_usuario`),
+  ADD KEY `fk_usuario_has_transacion_usuario2_idx` (`id_usuario2`);
+
+--
+-- Indices de la tabla `tipo_transacion`
+--
+ALTER TABLE `tipo_transacion`
+  ADD PRIMARY KEY (`id_transacion`),
+  ADD UNIQUE KEY `id_transacion_UNIQUE` (`id_transacion`);
 
 --
 -- Indices de la tabla `usuario`
@@ -69,29 +111,26 @@ ALTER TABLE `usuario`
   ADD UNIQUE KEY `id_usuario_UNIQUE` (`id_usuario`);
 
 --
--- Indices de la tabla `historial`
---
-ALTER TABLE `webbank`.`historial`
-  ADD PRIMARY KEY (`id_historial`,`id_usuario`),
-  ADD UNIQUE KEY `id_historial_UNIQUE` (`id_historial`),
-  ADD KEY `fk_usuario_has_transacion_usuario1_idx` (`id_usuario`);
-
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
-
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `historial`
 --
 ALTER TABLE `historial`
-  MODIFY `id_historial` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_historial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_transacion`
+--
+ALTER TABLE `tipo_transacion`
+  MODIFY `id_transacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
@@ -101,7 +140,9 @@ ALTER TABLE `historial`
 -- Filtros para la tabla `historial`
 --
 ALTER TABLE `historial`
-  ADD CONSTRAINT `fk_usuario_has_transacion_usuario1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_usuario_has_transacion_transacion1` FOREIGN KEY (`id_transacion`) REFERENCES `tipo_transacion` (`id_transacion`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_usuario_has_transacion_usuario1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_usuario_has_transacion_usuario2` FOREIGN KEY (`id_usuario2`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
